63e973e378b619a9571950686cd05150dbdbe404
Ishtiaque40 <ishtiahm06@gmail.com>
2026-09-09 02:35:59 +0600

    Create Comment.c

---
+++ W3School/Comment.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+//single line comment
+/* The code below will print the words Hello World!
+to the screen, and it is amazing */
+//printf("This will not print");
+printf("Hello World!");
+return 0;
+}
