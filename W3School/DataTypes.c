2ec5dc37a7dfb5fc1f24ba747cf2c46ad3182422
Ishtiaque40 <ishtiahm06@gmail.com>
2026-09-09 03:00:26 +0600

    DataTypes.c

---
+++ W3School/DataTypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int studentID = 2025331540;
+    float score = 3.75;
+    char grade = 'A';
+
+    printf("StudentID is %d", studentID);
+    printf("\nScore is %.2f", score);
+    printf("\nGrade is %c", grade);
+    return 0;
+}
