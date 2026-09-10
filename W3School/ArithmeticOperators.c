f4a8c5cc265287c27addbaca93f0d48ec19432f7
Ishtiaque40 <ishtiahm06@gmail.com>
2026-09-10 21:24:31 +0600

    ArithmeticOperators.c

---
+++ W3School/ArithmeticOperators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main () {
+  int x = 6, y = 6, z = 5;
+  printf("Addition: %d\n", x+y);
+  printf("Substraction: %d\n", x-y);
+  printf("Multiplication: %d\n", x*y);
+  printf("Division: %d\n", x/y);
+  printf("Remainder: %d\n", x%y);
+  ++z;
+  printf("Increament: %d\n", z);
+  --z;
+  printf("Decreament: %d\n", z);
+return 0;
+}
+
